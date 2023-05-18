9bb31621491d6c2f644cb6bb5ca628205d8b6503
nicugnm <nghergu17@gmail.com>
2023-05-18 12:13:30 +0300

    generate objectives component

--- healthy-run/src/app/app.module.ts
+++ healthy-run/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './login/login.component';
 import {FormsModule} from "@angular/forms";
+import { ObjectivesComponent } from './objectives/objectives.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    ObjectivesComponent
   ],
     imports: [
         BrowserModule,
---
+++ healthy-run/src/app/objectives/objectives.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-objectives',
+  templateUrl: './objectives.component.html',
+  styleUrls: ['./objectives.component.css']
+})
+export class ObjectivesComponent {
+
+}
